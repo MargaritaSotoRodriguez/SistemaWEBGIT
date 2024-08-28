3c62b833483f6e516e098e4b35f27561c37b71f3
MargaritaSotoRodriguez <margarita.soto@ues.mx>
2024-08-28 14:29:58 -0700

    Agregar archivos de proyecto.

---
+++ SistemaWEBGIT/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaWEBGIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
